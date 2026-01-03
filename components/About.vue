60d6587ae4b1f58dc3256bceedc7a18ee8389716
Rutvik Shah <rbshah0029@gmail.com>
2026-01-03 14:29:02 -0500

    Added new project
    - Added new resume

--- components/About.vue
+++ components/About.vue
@@ -6,15 +6,15 @@
             <p>
                 These days, I’m part of the team at 
                 <a href="https://www.evodev.ca/" target="_blank" class="text-blue-500">Évodév Technologies Inc.</a>, 
-                where I get to bring cool ideas to life across different projects. Nothing beats the feeling of turning a tricky problem into something people actually enjoy using.
+                where I get to bring cool ideas to life across different projects. I like turning a tricky problem into something people actually enjoy using.
             </p><br/>
 
             <p>
-                Along the way, I’ve worked with companies in supply chain management, open-source ERP systems, and full-stack development. Over the years, I’ve dabbled in all kinds of projects—web apps, mobile apps, APIs—you name it. I love collaborating, picking up new tech, and always hunting for smarter ways to build awesome stuff.
+                Along the way, I’ve worked with companies in supply chain management, open-source ERP systems, and full-stack development. Over the years, I’ve worked in all kinds of projects web apps, mobile apps, APIs you name it. I love collaborating, picking up new tech, and always looking for smarter ways to build.
             </p><br/>
 
             <p>
-                When I’m not coding, you’ll usually catch me watching movies, binging a good series, or getting lost in a book. At the end of the day, I’m all about creating software that makes a difference—while having fun doing it!
+                When I’m not coding, you’ll usually catch me watching movies, binging a good series, or reading a good book. At the end of the day, I’m all about creating software
             </p><br/>
 
             <p>
